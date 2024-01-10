05fd8196e5216cf8f35f6b89c1bd2d68ac733b74
Luis Miguel B <luibenmor@gmail.com>
2024-01-10 10:44:54 +0100

    Added first functions
    Creación de la función que crea un tablero vacío
    Creación de la función que muestra los tableros

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void tableroVacio(char[][] tablero) {
+
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                tablero[i][j] = '-';
+            }
+        }
+
+    }
+
+    public static void tableroMostrar(char[][] tablero) {
+
+        System.out.print(" \t");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+
+        char letra = 'A';
+        for (int i = 0; i < tablero.length; i++) {
+            System.out.print(letra + "\t");
+            letra++;
+            for (int j = 0; j < tablero.length; j++) {
+                System.out.print(tablero[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("Bienvenido a Battleship!");
+        char[][] tableroUser = new char[10][10];
+        System.out.println("Tablero vacío: ");
+        tableroVacio(tableroUser);
+        tableroMostrar(tableroUser);
+
     }
 }
