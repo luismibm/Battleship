8369988adc4052624db22ede8168127778978acc
LuisMiguelB <luibenmor@gmail.com>
2024-02-05 21:24:40 +0100

    V2.0
    El juego en la V1.0 no era funcional, ahora si
    
    Nuevo:
    Todos los tipos de barcos disponibles.
    Tiene en cuenta el tamaño del tablero antes de colocar un barco.
    Comprueba que la casilla introducida existe.
    
    Por mejorar:
    Al introducir las coordenadas solo acepta números, además de introducirse filas y columnas separado.
    La colocación de los barcos no falla pero se solapan.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
-import java.util.Random;
 
 public class Main {
 
-    // Menu Functions
+    // Game Functions
 
     public static int gameMode() {
 
@@ -17,6 +16,18 @@ public class Main {
 
     }
 
+    public static boolean checkWin(char[][] board, int cellsToWin) {
+        int count = 0;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                char cellContent = board[i][j];
+                if (cellContent == 'X') {
+                    count++;
+                }
+            }
+        }
+        return count == cellsToWin;
+    }
 
     // Board Functions
 
@@ -57,33 +68,90 @@ public class Main {
 
     // Boat Functions
 
-    public static boolean boatsLeft(char[][] gameBoard) {
-        for (int i = 0; i < gameBoard.length; i++) {
-            for (int j = 0; j < gameBoard[i].length; j++) {
-                char cellContent = gameBoard[i][j];
-                if (cellContent == 'L' || cellContent == 'B' || cellContent == 'A' || cellContent == 'P') {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    public static void addBoats(char[][] board, int numLanchas) {
+    public static void addBoats(char[][] board, int numLanchas, int numBuques, int numAcorazados, int numPortaaviones) {
 
         for (int i = 0; i < numLanchas; i++) {
             addLancha(board);
         }
 
+        for (int i = 0; i < numBuques; i++) {
+            addBuque(board);
+        }
+
+        for (int i = 0; i < numAcorazados; i++) {
+            addAcorazado(board);
+        }
+
+        for (int i = 0; i < numPortaaviones; i++) {
+            addPortaaviones(board);
+        }
+
     }
 
     public static void addLancha(char[][] board) {
-        int fila = (int) (Math.random() * 10);
-        int columna = (int) (Math.random() * 10);
-        if (board[fila][columna] == '-') {
-            board[fila][columna] = 'L';
+        int row = (int) (Math.random() * 10);
+        int col = (int) (Math.random() * 10);
+        if (board[row][col] == '-') {
+            board[row][col] = 'L';
         } else {
             addLancha(board);
+        }
+    }
+
+    public static void addBuque(char[][] board) {
+        int row = (int) (Math.random() * 10);
+        int col = (int) (Math.random() * 10);
+        if (board[row][col] == '-') {
+            board[row][col] = 'B';
+            if (col < 8) {
+                board[row][col + 1] = 'B';
+                board[row][col + 2] = 'B';
+            } else {
+                board[row][col - 1] = 'B';
+                board[row][col - 2] = 'B';
+            }
+        } else {
+            addBuque(board);
+        }
+    }
+
+    public static void addAcorazado(char[][] board) {
+        int row = (int) (Math.random() * 10);
+        int col = (int) (Math.random() * 10);
+        if (board[row][col] == '-') {
+            board[row][col] = 'A';
+            if (col < 7) {
+                board[row][col + 1] = 'A';
+                board[row][col + 2] = 'A';
+                board[row][col + 3] = 'A';
+            } else {
+                board[row][col - 1] = 'A';
+                board[row][col - 2] = 'A';
+                board[row][col - 3] = 'A';
+            }
+        } else {
+            addAcorazado(board);
+        }
+    }
+
+    public static void addPortaaviones(char[][] board) {
+        int row = (int) (Math.random() * 10);
+        int col = (int) (Math.random() * 10);
+        if (board[row][col] == '-') {
+            board[row][col] = 'P';
+            if (row < 6) {
+                board[row + 1][col] = 'P';
+                board[row + 2][col] = 'P';
+                board[row + 3][col] = 'P';
+                board[row + 4][col] = 'P';
+            } else {
+                board[row - 1][col] = 'P';
+                board[row - 2][col] = 'P';
+                board[row - 3][col] = 'P';
+                board[row - 4][col] = 'P';
+            }
+        } else {
+            addPortaaviones(board);
         }
     }
 
@@ -92,13 +160,23 @@ public class Main {
 
     public static void openFire(char[][] sBoard, char[][] hBoard, int row, int col) {
 
-        if (hBoard[row][col] == '-') {
-            sBoard[row][col] = '*';
+        if (row >= 0 && row < 10 && col >= 0 && col < 10) {
+            if (hBoard[row][col] == '-') {
+                sBoard[row][col] = '*';
+            } else {
+                sBoard[row][col] = 'X';
+            }
         } else {
-            sBoard[row][col] = 'X';
+            System.out.println("Invalid cell");
         }
 
+        // Primer if comprueba que la celda está dentro del tablero
+        // Segundo if comprueba si la celda está vacía o tiene un barco
+
     }
+
+
+    // Main
 
     public static void main(String[] args){
 
@@ -107,32 +185,50 @@ public class Main {
         char[][] shownBoard = createBoard(10,10);
         char[][] hiddenBoard = createBoard(10,10);
 
+        int cellsToWin = 0;
+
         switch (gameMode()) {
 
             case 1:
-                addBoats(hiddenBoard, 5);
+                addBoats(hiddenBoard, 5, 3, 1, 1);
+                cellsToWin = 23;
                 break;
 
             case 2:
-                addBoats(hiddenBoard, 3);
+                addBoats(hiddenBoard, 3, 1, 1, 1);
+                cellsToWin = 15;
                 break;
 
             case 3:
-                addBoats(hiddenBoard, 1);
+                addBoats(hiddenBoard, 1, 1, 0, 0);
+                cellsToWin = 4;
                 break;
 
         }
 
-        // Test
-
+        showBoard(hiddenBoard);
         int fireOpportunities = 50;
+        boolean gameWon = false;
         do {
             showBoard(shownBoard);
             openFire(shownBoard, hiddenBoard, sc.nextInt(), sc.nextInt());
             fireOpportunities--;
-            System.out.println("Fire opportunities left: " + fireOpportunities);
-        } while (boatsLeft(hiddenBoard) && fireOpportunities > 0);
 
+            if (checkWin(shownBoard, cellsToWin) && fireOpportunities > 0) {
+                System.out.println("You win!");
+                gameWon = true;
+            } else if (fireOpportunities == 0){
+                System.out.println("You lose!");
+            }
+
+            if (!gameWon) {
+                System.out.println("Fire opportunities left: " + fireOpportunities);
+            }
+
+        } while (!gameWon && fireOpportunities > 0);
 
     }
+
+    //
+
 }
